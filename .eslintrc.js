79b8c8da09b402173bb03866e2f742b585aa1bb6
Lubos Fukas <lubos.fukas@gmail.com>
2022-04-25 16:14:32 +0200

    chore: :wrench: add eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,11 +10,13 @@ module.exports = {
   },
   plugins: ["react", "@typescript-eslint"],
   rules: {
-    // supress errors for the use of dev packages
     "import/no-extraneous-dependencies": ["error", { devDependencies: true }],
-    // supress errors for missing 'import React' in files
+    "import/prefer-default-export": "off",
+    "react/function-component-definition": [
+      2,
+      { namedComponents: "arrow-function", unnamedComponents: "arrow-function" },
+    ],
     "react/react-in-jsx-scope": "off",
-    // supress errors for not allowed files with extension
     "react/jsx-filename-extension": [2, { extensions: [".js", ".jsx", ".ts", ".tsx"] }],
   },
 };
