debbb9cca1f6ed69ee0822ee718563680f01a49e
Lubos Fukas <lubos.fukas@gmail.com>
2022-10-22 18:12:44 +0200

    chore: :clown_face: mock new category queries

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,10 +1,32 @@
 import { rest } from "msw";
 
-import { fetchAccountsUrl, fetchCategoriesUrl, fetchTransactionsUrl } from "../hooks";
+import { Category } from "../types";
+import { API_URL } from "../utils/constants";
 import { accounts, categories, transactions } from "../utils/mockedData";
 
+const getRandomId = (min = 0, max = 1000) => Math.random() * (max - min) + min;
+
 export const handlers = [
-  rest.get(fetchAccountsUrl, (_, res, ctx) => res(ctx.status(200), ctx.json(accounts))),
-  rest.get(fetchCategoriesUrl, (_, res, ctx) => res(ctx.status(200), ctx.json(categories))),
-  rest.get(fetchTransactionsUrl, (_, res, ctx) => res(ctx.status(200), ctx.json(transactions))),
+  rest.get(`${API_URL}/accounts`, (_, res, ctx) => res(ctx.delay(500), ctx.status(200), ctx.json(accounts))),
+  rest.get(`${API_URL}/transactions`, (_, res, ctx) => res(ctx.status(200), ctx.json(transactions))),
+  rest.get(`${API_URL}/categories`, (_, res, ctx) => res(ctx.delay(1000), ctx.status(200), ctx.json(categories))),
+  rest.get(`${API_URL}/categories/:id`, (req, res, ctx) => {
+    const { id } = req.params;
+    const category = categories.find(({ id: categoryId }) => id === categoryId.toString());
+
+    return res(ctx.status(200), ctx.json(category));
+  }),
+  rest.post(`${API_URL}/categories`, (req, res, ctx) => {
+    const category = req.body as Category;
+
+    return res(ctx.status(201), ctx.json({ ...category, id: getRandomId() }));
+  }),
+  rest.patch(`${API_URL}/categories/:id`, (req, res, ctx) => {
+    const { id } = req.params;
+    const { label } = req.body as Category;
+    const category = categories.find(({ id: categoryId }) => id === categoryId.toString());
+
+    return res(ctx.delay(500), ctx.status(200), ctx.json({ ...category, label }));
+  }),
+  rest.delete(`${API_URL}/categories/:id`, (_, res, ctx) => res(ctx.delay(500), ctx.status(200))),
 ];
--- src/utils/mockedData.ts
+++ src/utils/mockedData.ts
@@ -8,19 +8,19 @@ export const accounts: Array<Account> = [
 const visaAccount = accounts[0];
 
 export const categories: Array<Category> = [
-  { id: 1, label: "Food", name: "food", type: "EXPENSES" },
-  { id: 2, label: "Healthcare", name: "healtcare", type: "EXPENSES" },
-  { id: 3, label: "Insurance", name: "insurance", type: "EXPENSES" },
-  { id: 4, label: "Investments", name: "investments", type: "EXPENSES" },
-  { id: 5, label: "Housing", name: "housing", type: "EXPENSES" },
-  { id: 6, label: "Personal spendings", name: "personalSpendings", type: "EXPENSES" },
-  { id: 7, label: "Recreation & Entertainment", name: "recreationEntertainment", type: "EXPENSES" },
-  { id: 8, label: "Savings", name: "savings", type: "EXPENSES" },
-  { id: 9, label: "Transportation", name: "transportation", type: "EXPENSES" },
-  { id: 10, label: "Utilities", name: "utilities", type: "EXPENSES" },
-  { id: 11, label: "Salary", name: "salary", type: "INCOMES" },
-  { id: 12, label: "Gift", name: "gift", type: "INCOMES" },
-  { id: 13, label: "Refunds", name: "refunds", type: "INCOMES" },
+  { id: 1, label: "Food", type: "EXPENSES" },
+  { id: 2, label: "Healthcare", type: "EXPENSES" },
+  { id: 3, label: "Insurance", type: "EXPENSES" },
+  { id: 4, label: "Investments", type: "EXPENSES" },
+  { id: 5, label: "Housing", type: "EXPENSES" },
+  { id: 6, label: "Personal spendings", type: "EXPENSES" },
+  { id: 7, label: "Recreation & Entertainment", type: "EXPENSES" },
+  { id: 8, label: "Savings", type: "EXPENSES" },
+  { id: 9, label: "Transportation", type: "EXPENSES" },
+  { id: 10, label: "Utilities", type: "EXPENSES" },
+  { id: 11, label: "Salary", type: "INCOMES" },
+  { id: 12, label: "Gift", type: "INCOMES" },
+  { id: 13, label: "Refunds", type: "INCOMES" },
 ];
 
 const foodCategory = categories[0];
