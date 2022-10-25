968ccbcfbf41b723219adf5fffbb2291e2862f42
Lubos Fukas <lubos.fukas@gmail.com>
2022-10-25 20:39:36 +0200

    chore: :clown_face: msw account handlers

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,37 +1,58 @@
 import { rest } from "msw";
 
-import { Category, TransactionCreatePayload } from "../types";
+import { Account, Category, TransactionCreatePayload } from "../types";
 import { API_URL } from "../utils/constants";
 import { accounts, categories, transactions } from "../utils/mockedData";
 
 const getRandomId = (min = 0, max = 1000) => Math.floor(Math.random() * (max - min) + min);
 
-export const handlers = [
+const accountHandlers = [
   rest.get(`${API_URL}/accounts`, (_, res, ctx) => res(ctx.delay(500), ctx.status(200), ctx.json(accounts))),
-  rest.get(`${API_URL}/transactions`, (_, res, ctx) => res(ctx.status(200), ctx.json(transactions))),
-  rest.post(`${API_URL}/transactions`, (req, res, ctx) => {
-    const transaction = req.body as TransactionCreatePayload;
-
-    return res(ctx.status(200), ctx.json({ ...transaction, id: getRandomId() }));
+  rest.get(`${API_URL}/accounts/:id`, (req, res, ctx) => {
+    const { id } = req.params;
+    const account = accounts.find(({ id: accountId }) => id === accountId.toString());
+    return res(ctx.delay(500), ctx.status(200), ctx.json(account));
   }),
-  rest.get(`${API_URL}/categories`, (_, res, ctx) => res(ctx.delay(1000), ctx.status(200), ctx.json(categories))),
+  rest.post(`${API_URL}/accounts`, (req, res, ctx) => {
+    const account = req.body as Account;
+    return res(ctx.delay(500), ctx.status(201), ctx.json(account));
+  }),
+  rest.patch(`${API_URL}/accounts/:id`, (req, res, ctx) => {
+    const { id } = req.params;
+    const { label } = req.body as Account;
+    const account = accounts.find(({ id: accountId }) => id === accountId.toString());
+    return res(ctx.delay(500), ctx.status(200), ctx.json({ ...account, label }));
+  }),
+  rest.delete(`${API_URL}/accounts/:id`, (_, res, ctx) => res(ctx.delay(500), ctx.status(200))),
+];
+
+const categoryHandlers = [
+  rest.get(`${API_URL}/categories`, (_, res, ctx) => res(ctx.delay(500), ctx.status(200), ctx.json(categories))),
   rest.get(`${API_URL}/categories/:id`, (req, res, ctx) => {
     const { id } = req.params;
     const category = categories.find(({ id: categoryId }) => id === categoryId.toString());
-
-    return res(ctx.status(200), ctx.json(category));
+    return res(ctx.delay(500), ctx.status(200), ctx.json(category));
   }),
   rest.post(`${API_URL}/categories`, (req, res, ctx) => {
     const category = req.body as Category;
-
-    return res(ctx.status(201), ctx.json({ ...category, id: getRandomId() }));
+    return res(ctx.delay(500), ctx.status(201), ctx.json({ ...category, id: getRandomId() }));
   }),
   rest.patch(`${API_URL}/categories/:id`, (req, res, ctx) => {
     const { id } = req.params;
     const { label } = req.body as Category;
     const category = categories.find(({ id: categoryId }) => id === categoryId.toString());
-
     return res(ctx.delay(500), ctx.status(200), ctx.json({ ...category, label }));
   }),
   rest.delete(`${API_URL}/categories/:id`, (_, res, ctx) => res(ctx.delay(500), ctx.status(200))),
+];
+
+export const handlers = [
+  ...accountHandlers,
+  ...categoryHandlers,
+
+  rest.get(`${API_URL}/transactions`, (_, res, ctx) => res(ctx.status(200), ctx.json(transactions))),
+  rest.post(`${API_URL}/transactions`, (req, res, ctx) => {
+    const transaction = req.body as TransactionCreatePayload;
+    return res(ctx.status(200), ctx.json({ ...transaction, id: getRandomId() }));
+  }),
 ];
