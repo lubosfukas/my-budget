3a68f887f5f9f98aeaa099673c159428ab07b607
Lubos Fukas <lubos.fukas@gmail.com>
2022-12-03 18:25:38 +0100

    feat: :sparkles: set up cors for api

--- my-budget_api/app.js
+++ my-budget_api/app.js
@@ -1,4 +1,5 @@
 const createError = require("http-errors");
+const cors = require("cors");
 const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
@@ -20,6 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(cors());
 
 app.use("/", indexRouter);
 app.use("/accounts", accountsRouter);
