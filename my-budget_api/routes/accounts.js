9a13dc588dd69365684196a6597cb4ee6df25c32
Lubos Fukas <lubos.fukas@gmail.com>
2022-12-08 13:45:02 +0100

    feat: :card_file_box: use mongodb account document

--- my-budget_api/routes/accounts.js
+++ my-budget_api/routes/accounts.js
@@ -1,36 +1,57 @@
-const { getRandomId } = require("../utils/helpers");
-const { accounts } = require("../utils/mockedData");
-
 const express = require("express");
+const mongoose = require("mongoose");
+
 const router = express.Router();
 
-router.get("/", function (_, res) {
+const accountSchema = new mongoose.Schema({
+  initialBalance: "number",
+  label: "string",
+});
+accountSchema.virtual("id").get(function () {
+  return this._id.toHexString();
+});
+accountSchema.set("toJSON", {
+  virtuals: true,
+  versionKey: false,
+  transform: function (_, res) {
+    delete res._id;
+  },
+});
+
+const Account = mongoose.model("Model", accountSchema, "account");
+
+router.get("/", async function (_, res) {
+  const accounts = await Account.find({});
   res.status(200).send(accounts);
 });
 
-router.get("/:id", function (req, res) {
+router.get("/:id", async function (req, res) {
   const { id } = req.params;
-  const account = accounts.find(({ id: accountId }) => id === accountId.toString());
+  const account = await Account.findById(id);
 
   if (account) res.status(200).send(account);
   else res.sendStatus(404);
 });
 
-router.post("/", function (req, res) {
-  res.status(201).send({ ...req.body, id: getRandomId() });
+router.post("/", async function (req, res) {
+  await Account.create(req.body)
+    .then((account) => res.status(201).send(account))
+    .catch(() => res.sendStatus(400));
 });
 
-router.patch("/:id", function (req, res) {
+router.patch("/:id", async function (req, res) {
   const { id } = req.params;
   const body = req.body;
-  const account = accounts.find(({ id: accountId }) => id === accountId.toString());
-
-  if (account) res.status(200).send({ ...account, ...body });
-  else res.sendStatus(404);
+  await Account.findByIdAndUpdate(id, body, { returnDocument: "after" })
+    .then((account) => res.status(200).send(account))
+    .catch(() => res.sendStatus(400));
 });
 
-router.delete("/:id", function (_, res) {
-  res.sendStatus(204);
+router.delete("/:id", async function (req, res) {
+  const { id } = req.params;
+  await Account.findByIdAndRemove({ _id: id })
+    .then(() => res.sendStatus(204))
+    .catch(() => res.sendStatus(404));
 });
 
 module.exports = router;
