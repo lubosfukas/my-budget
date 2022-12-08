e15c8f603a7f190aae5a057ee6c86f672bee339e
Lubos Fukas <lubos.fukas@gmail.com>
2022-12-08 12:19:00 +0100

    refactor: :alien: disable msw

--- my-budget_frontend/src/main.tsx
+++ my-budget_frontend/src/main.tsx
@@ -3,12 +3,7 @@ import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import { worker } from "./mocks/browser";
 import App from "./App";
-
-if (process.env.NODE_ENV === "development") {
-  worker.start();
-}
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
